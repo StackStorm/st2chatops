1512f9bd1a01fa5ffee451ffc0e15bf4e7b9bdc3
blag <blag@users.noreply.github.com>
2020-07-25 16:59:13 -0700

    Explain the flags to dpkg-buildpackage

--- packagingenv/xenial/docker-entrypoint.sh
+++ packagingenv/xenial/docker-entrypoint.sh
@@ -6,7 +6,14 @@ case "$operation" in
 pull)
   ;;
 build)
+  # Updates the changelog by running:
+  #
+  #     debchange -v $(PKG_VERSION)-$(PKG_RELEASE) -M ""
   make changelog
+  # Runs the sign hook
+  # -b  - binary-only package
+  # -uc - don't sign the .changes file
+  # -us - don't sign the .dsc file
   dpkg-buildpackage -b -uc -us
   cp ../*.{deb,changes} $ARTIFACT_DIR
   ;;
