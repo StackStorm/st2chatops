79f90f2450e77c76cf32cd9b955a614abd298ed9
Jacob Zufelt <jzufelt@adobe.com>
2025-04-03 10:03:05 -0600

    Update envvars

--- testingenv/rockylinux9/docker-entrypoint.sh
+++ testingenv/rockylinux9/docker-entrypoint.sh
@@ -12,7 +12,8 @@ test)
   yum install -y $ARTIFACT_DIR/*.rpm
   cd /opt/stackstorm/chatops
   sed -i.bak -r "s/^# (export HUBOT_ADAPTER=slack)/\1/" st2chatops.env
-  sed -i.bak -r "s/^# (export HUBOT_SLACK_TOKEN.).*/\1$SLACK_TOKEN/" st2chatops.env
+  sed -i.bak -r "s/^# (export HUBOT_SLACK_BOT_TOKEN.).*/\1$SLACK_TOKEN/" st2chatops.env
+  sed -i.bak -r "s/^# (export HUBOT_SLACK_APP_TOKEN.).*/\1$SLACK_APP_TOKEN/" st2chatops.env
   sed -i.bak -r "s/^(export ST2_AUTH_USERNAME.).*/\1$ST2_USERNAME/" st2chatops.env
   sed -i.bak -r "s/^(export ST2_AUTH_PASSWORD.).*/\1$ST2_PASSWORD/" st2chatops.env
   bin/hubot &> /tmp/hubot.log &
